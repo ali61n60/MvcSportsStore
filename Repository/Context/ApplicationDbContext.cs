3ceb7ab3dc879b18104054b791447a171fec3569
Ali <ali62n62@yahoo.com>
2020-05-01 17:29:56 +0430

    EntityFramework Added

---
+++ Repository/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
