d28fca70edfbaf599e8459e4aa0e4edd2e9fa274
Ali <ali62n62@yahoo.com>
2020-05-01 16:55:13 +0430

    views added

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.IRepositories;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+    }
+}
