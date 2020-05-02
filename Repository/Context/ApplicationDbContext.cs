4a7ee30051ea7ad0e22741d4d35b0f1c18fc7a7b
anejati <anejati@115Q216Q>
2020-05-02 15:24:18 +0430

    adding microsoft.entityframeworkcore

---
+++ Repository/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
