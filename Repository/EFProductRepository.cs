258f1bd68bd2b7f9d47931d23977bfdf18567a26
anejati <anejati@115Q216Q>
2020-05-02 16:02:04 +0430

    adding appsetting to mvc project

---
+++ Repository/EFProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using Models.IRepositories;
+using Repository.Context;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    public class EFProductRepository:IProductRepository
+    {
+        private ApplicationDbContext context;
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IEnumerable<Product> Products => context.Products;
+    }
+}
+
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -4,16 +4,29 @@ using Microsoft.Extensions.DependencyInjection;
 using Repository;
 using Models;
 using Models.IRepositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Repository.Context;
 
 namespace SportStore
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        IConfigurationRoot Configuration;
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json").Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                Configuration["Data:SportStoreProducts:ConnectionString"]));
+
+            services.AddTransient<IProductRepository, EFProductRepository>();           
             services.AddMvc();
         }
 
