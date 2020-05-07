b2f1ba9aa0415a751191de69f751fe43ea00ad10
ali <ali@ALI-PC>
2020-05-07 19:28:42 +0430

    working with view components

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -35,17 +35,33 @@ namespace SportStore
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvc(routes => {
-                routes.MapRoute(name: "pagination",
-                    template: "Products/Page{page}",
-                    defaults: new { Controller = "Product", action = "List" });
 
-                routes.MapRoute(name: "default",
-                template: "{controller=Product}/{action=List}/{id?}");
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}/Page{page:int}",
+                    defaults: new { controller = "Product", action = "List" });
+
+                routes.MapRoute(
+                name: null,
+                template: "Page{page:int}",
+                defaults: new { controller = "Product", action = "List", page = 1 }
+                );
+
+                routes.MapRoute(
+                name: null,
+                template: "{category}",
+                defaults: new { controller = "Product", action = "List", page = 1 }
+                );
+
+                routes.MapRoute(
+                name: null,
+                template: "",
+                defaults: new { controller = "Product", action = "List", page = 1 });
+
+                routes.MapRoute(name: null, template: "{controller}/{action}/{id?}");
             });
 
             SeedData.EnsurePopulated(app);
-
-
 
         }
     }
