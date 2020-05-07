4be715dc868f309da93c5515dd4b71ebf23537bb
ali <ali@ALI-PC>
2020-05-07 18:38:19 +0430

    updating nunit

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -30,7 +30,9 @@ namespace SportsStore.Controllers
             {
                 CurrentPage = page,
                 ItemsPerPage = PageSize,
-                TotalItems = repository.Products.Count()
+                TotalItems = repository.Products
+                .Where(p => category == null || p.Category == category)
+                .Count()
             };
             productsListViewModel.CurrentCategory = category;
 
